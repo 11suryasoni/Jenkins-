7e38dd3071430da57c026efff5d72552875ef318
Surya Prakash Soni <suryasonirewa@gmail.com>
2022-11-17 14:33:41 +0530

    Hello World Program

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+package com.azuga.demo.controller;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World !!! This is my first Jenkins Hello World Testing Code");
+    }
+}
